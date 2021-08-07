39d4eaa985016b35031f52fc454b0557d12a67d7
Joel Rojas <jr.bestone17@gmail.com>
2021-08-07 19:33:08 -0400

    Add creation of prerequisites function

--- cypress/integration/asset/create-asset.js
+++ cypress/integration/asset/create-asset.js
@@ -1,46 +1,83 @@
-import {clickField} from '../../src/core/action'
 import {createAsset} from '../../src/salesforce/asset/new-asset'
+import {login} from '../../src/core/action'
 import {pageTransporter} from '../../src/salesforce/transporter'
 import {validateAsset} from '../../src/salesforce/asset/validate-asset'
 const endpoint = require('../../fixtures/endpoint/endpoint.json')
 const assets = require('../../fixtures/locator/asset/assets.json')
 
+const feature = require('../../src/salesforce/api/features')
+const apiLogin = require("../../src/salesforce/api/login")
+const allAttrAsset = require('../../fixtures/features/asset/all-attributes.json')
+const necessaryAttrAsset = require('../../fixtures/features/asset/necessary-attributes.json')
+
 describe('Create an Asset', () => {
+
+    let token = ''
+    let accountId = ''
+    let contactId = ''
+    let productId = ''
+    before(async () => {
+        token = await apiLogin.login()
+    })
 
     beforeEach('navigate to salesforce login page', () => {
         pageTransporter("/")
-        cy.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
+        login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
         pageTransporter(endpoint.asset)
-        clickField(assets.newAssetBtn)
+        cy.clickField(assets.newAssetBtn)
     })
 
     it('should create it with necessary attributes', () => {
-        const asset = {
-            "account": 'cuenta 13',
-            "name": 'New Asset'
+        const asset = necessaryAttrAsset
+        const account = {
+            "Name": asset.account
         }
+        feature.create("Account", token, account).then((response) => {
+            accountId = response.body.id
+        }).then(() => {
         createAsset(asset)
 
         validateAsset(asset)
+        })
     })
 
     it('should create it with all attributes', () => {
-        const asset = {
-            "account": 'cuenta 13',
-            "competitor": true,
-            "description": 'Description',
-            "install date": '28/7/2021',
-            "name": 'New Asset',
-            "price": '100',
-            "product": 'producto 2',
-            "purchase date": '15/8/2021',
-            "quantity": '10,00',
-            "serial number": 'Serial Number',
-            "status": 'Shipped',
-            "usage end date": '30/9/2021'
+        const asset = allAttrAsset
+        const account = {
+            "Name": asset.account
         }
+        const product = {
+            "Name": asset.product
+        }
+        feature.create("Account", token, account).then((response) => {
+            accountId = response.body.id
+            const contact = {
+                "LastName": asset.contact,
+                "AccountId": accountId
+            }
+            feature.create("Contact", token, contact).then((response2) => {
+                contactId = response2.body.id
+            })
+            feature.create("Product2", token, product).then((response3) => {
+                productId = response3.body.id
+            })
+        }).then(() => {
+
         createAsset(asset)
 
         validateAsset(asset)
+        })
+    })
+
+    afterEach(() => {
+        if (contactId !== '') {
+            feature.deleteOne("Contact", token, contactId)
+        }
+        if (accountId !== '') {
+            feature.deleteOne("Account", token, accountId)
+        }
+        if (productId !== '') {
+            feature.deleteOne("Product2", token, productId)
+        }
     })
 })
--- cypress/src/salesforce/asset/validate-table.js
+++ cypress/src/salesforce/asset/validate-table.js
@@ -1,15 +1,14 @@
 import {getJsonEntityAttributes} from '../../core/entity-actions'
 import {iterateSetToRunMap} from '../../core/map-actions'
-import {validateTextInField} from '../../core/action'
 const detailAsset = require('../../../fixtures/locator/asset/assets.json')
 
 function setValidationAssetMap(asset, id) {
     const assetMap = new Map()
-    assetMap.set("name", () => validateTextInField(id + detailAsset.assetNameTbTxt, asset.name))
-    assetMap.set("account", () => validateTextInField(id + detailAsset.accountTbTxt, asset.account))
-    assetMap.set("product", () => validateTextInField(id + detailAsset.productTbTxt, asset.product))
-    assetMap.set("serial number", () => validateTextInField(id + detailAsset.serialNumberTbTxt, asset["serial number"]))
-    assetMap.set("install date", () => validateTextInField(id + detailAsset.installDateTbTxt, asset["install date"]))
+    assetMap.set("name", () => cy.validateTextInField(id + detailAsset.assetNameTbTxt, asset.name))
+    assetMap.set("account", () => cy.validateTextInField(id + detailAsset.accountTbTxt, asset.account))
+    assetMap.set("contact", () => cy.validateTextInField(id + detailAsset.contactTbTxt, asset.contact))
+    assetMap.set("serial number", () => cy.validateTextInField(id + detailAsset.serialNumberTbTxt, asset["serial number"]))
+    assetMap.set("install date", () => cy.validateTextInField(id + detailAsset.installDateTbTxt, asset["install date"]))
     return assetMap
 }
 
