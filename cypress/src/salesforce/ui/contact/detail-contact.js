ecc3613c65f98adc2303e6b7a23ea8f8e434c09e
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-08-12 15:16:26 -0400

    change validatecontact method

--- cypress/src/salesforce/ui/contact/detail-contact.js
+++ cypress/src/salesforce/ui/contact/detail-contact.js
@@ -1,22 +1,12 @@
 const detailContact = require("../../../../fixtures/locator/contact/detail-contact.json");
-import {getJsonEntityAttributes} from '../../../core/entity-actions'
-import {iterateSetToRunMap} from '../../../core/map-actions'
 const {getCurrentDate} = require('../../../utils/formatDate')
 const {createContactName} = require('../../../utils/formContactName')
 
-function setValidationContactMap(contactData) {
-  const contactMap = new Map()
-  const createdBy = getCurrentDate()
-  const fullName = createContactName(contactData.salutation, contactData.firstName, contactData.lastName)
-  contactMap.set("topName", () => cy.validateConteinTextInField(detailContact.topName, fullName))
-  contactMap.set("detailName", () => cy.validateConteinTextInField(detailContact.detailName, fullName))
-  contactMap.set("createdBy", () => cy.validateConteinTextInField(detailContact.createdBy, createdBy))
-  contactMap.set("contact", () => cy.validateConteinTextInField(detailContact.lastModifiedBy, createdBy))
-  return contactMap
-}
-
 export function validateContact(contactData) {
-  const contactMap = setValidationContactMap(contactData)
-  const ContactSet = getJsonEntityAttributes(contactData)
-  iterateSetToRunMap(contactMap, ContactSet)
+  const createdBy = getCurrentDate().substring(0, 15)
+  const fullName = createContactName(contactData.salutation, contactData.firstName, contactData.lastName)
+  cy.validateConteinTextInField(detailContact.topName, fullName)
+  cy.validateConteinTextInField(detailContact.detailName, fullName)
+  cy.validateConteinTextInField(detailContact.createdBy, createdBy)
+  cy.validateConteinTextInField(detailContact.lastModifiedBy, createdBy)
 }
